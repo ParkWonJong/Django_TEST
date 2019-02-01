b590308f46815db27a89aeca4ba1149dbd9c60bc
ParkWonJong <dnjswhd403@naver.com>
2019-02-01 16:05:29 +0900

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+def index(request):
+    return render(request, "index.html")
